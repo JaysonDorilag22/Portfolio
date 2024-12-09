f060633ae4a0ec5c447e9496b16b96b89f0bcddd
JD <jaysondorilag1@gmail.com>
2024-12-09 16:47:07 +0800

    initialize React app with Vite, Tailwind CSS, and essential configurations

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'ms_sans_serif': ['"Ms Sans Serif"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
